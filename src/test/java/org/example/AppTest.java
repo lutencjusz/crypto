8e2df63d1dcfb16b34e8019936344ab330c5b657
lutencjusz <lutencjusz@gmail.com>
2022-05-03 10:47:55 +0200

    add AES

--- src/test/java/org/example/AppTest.java
+++ src/test/java/org/example/AppTest.java
@@ -4,48 +4,44 @@ import static org.example.utils.CryptoText.*;
 
 import org.apache.commons.codec.binary.Base64;
 import org.example.utils.ConsoleColors;
+import org.joda.time.LocalTime;
 import org.junit.Assert;
 import org.junit.Test;
 
+import java.util.Date;
+import java.util.concurrent.TimeUnit;
+
+
 public class AppTest {
+
+    public void testAlgorithm(String algorithm) {
+        System.out.println(ConsoleColors.GREEN_BOLD + "Testowanie " + algorithm + ConsoleColors.RESET);
+        String testData = testPassGenerator((int) (10 + Math.round(Math.random() * 10)));
+        Long encodeBegin = System.nanoTime();
+        String encodedData = encode(testData, algorithm);
+        Long encodeEnd = System.nanoTime();
+        double diffEncode = (double) (encodeEnd - encodeBegin) / 1_000_000;
+        assert encodedData != null;
+        String encodedDataB64 = new String(Base64.decodeBase64(encodedData.getBytes()));
+        Long decodeBegin = System.nanoTime();
+        String decodedData = decode(encodedData, algorithm);
+        Long decodeEnd = System.nanoTime();
+        double diffDecode = (double) (decodeEnd - decodeBegin) / 1_000_000;
+        System.out.println("Tekst testowy: " + ConsoleColors.WHITE_BOLD_BRIGHT + testData + ConsoleColors.RESET);
+        System.out.println("Tekst zakodowany " + algorithm + ": " + encodedData + ", czas kodowania: " + diffEncode + " min. sek.");
+        System.out.println("Tekst zdekodowany Base64: " + encodedDataB64);
+        System.out.println("Tekst zdekodowany " + algorithm + ": " + ConsoleColors.WHITE_BOLD_BRIGHT + decodedData + ConsoleColors.RESET + " czas dekodowania: " + diffDecode + " min. sek.");
+        Assert.assertEquals(ConsoleColors.RED_BOLD + "Dane testowe '" + testData + "' i '" + decodedData + "' nie są zgodne" + ConsoleColors.RED_BOLD, testData, decodedData);
+        Assert.assertNotEquals(ConsoleColors.RED_BOLD + "Dane testowe '" + testData + "' i '" + decodedData + "' są zgodne, a nie powinny" + ConsoleColors.RED_BOLD, decodedData, encodedDataB64);
+    }
 
     @Test
     public void testDES() {
-        try {
-            System.out.println(ConsoleColors.GREEN_BOLD + "Testowanie DES" + ConsoleColors.RESET);
-            String testData = testPassGenerator((int) (10 + Math.round(Math.random() * 10)));
-            String encodedData = encode(testData, "DES");
-            assert encodedData != null;
-            String encodedDataB64 = new String(Base64.decodeBase64(encodedData.getBytes()));
-            String decodedData = decode(encodedData, "DES");
-            System.out.println("Tekst testowy: " + ConsoleColors.WHITE_BOLD_BRIGHT + testData + ConsoleColors.RESET);
-            System.out.println("Tekst zakodowany DES: " + encodedData);
-            System.out.println("Tekst zdekodowany Base64: " + encodedDataB64);
-            System.out.println("Tekst zdekodowany DES: " + ConsoleColors.WHITE_BOLD_BRIGHT + decodedData + ConsoleColors.RESET);
-            Assert.assertEquals(ConsoleColors.RED_BOLD + "Dane testowe '" + testData + "' i '" + decodedData + "' nie są zgodne" + ConsoleColors.RED_BOLD, testData, decodedData);
-            Assert.assertNotEquals(ConsoleColors.RED_BOLD + "Dane testowe '" + testData + "' i '" + decodedData + "' są zgodne, a nie powinny" + ConsoleColors.RED_BOLD, decodedData, encodedDataB64);
-        } catch (Exception ex) {
-            System.err.println(ConsoleColors.RED_BOLD + "Wystąpił błąd:" + ex.getMessage() + ConsoleColors.RESET);
-        }
+        testAlgorithm("DES");
     }
 
     @Test
     public void testAES() {
-        try {
-            System.out.println(ConsoleColors.GREEN_BOLD + "Testowanie AES" + ConsoleColors.RESET);
-            String testData = testPassGenerator((int) (10 + Math.round(Math.random() * 10)));
-            String encodedData = encode(testData, "AES");
-            assert encodedData != null;
-            String encodedDataB64 = new String(Base64.decodeBase64(encodedData.getBytes()));
-            String decodedData = decode(encodedData, "AES");
-            System.out.println("Tekst testowy: " + ConsoleColors.WHITE_BOLD_BRIGHT + testData + ConsoleColors.RESET);
-            System.out.println("Tekst zakodowany AES: " + encodedData);
-            System.out.println("Tekst zdekodowany Base64: " + encodedDataB64);
-            System.out.println("Tekst zdekodowany AES: " + ConsoleColors.WHITE_BOLD_BRIGHT + decodedData + ConsoleColors.RESET);
-            Assert.assertEquals(ConsoleColors.RED_BOLD + "Dane testowe '" + testData + "' i '" + decodedData + "' nie są zgodne" + ConsoleColors.RED_BOLD, testData, decodedData);
-            Assert.assertNotEquals(ConsoleColors.RED_BOLD + "Dane testowe '" + testData + "' i '" + decodedData + "' są zgodne, a nie powinny" + ConsoleColors.RED_BOLD, decodedData, encodedDataB64);
-        } catch (Exception ex) {
-            System.err.println(ConsoleColors.RED_BOLD + "Wystąpił błąd:" + ex.getMessage() + ConsoleColors.RESET);
-        }
+        testAlgorithm("AES");
     }
 }
